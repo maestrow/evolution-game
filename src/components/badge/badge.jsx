eb2fed9acc9da91dce30288622841c006e1d90be
maestrow <andrew.shmelev@gmail.com>
2018-06-16 00:45:06 +0300

    change badge style

--- src/components/badge/badge.jsx
+++ src/components/badge/badge.jsx
@@ -30,14 +30,14 @@ class Badge extends React.Component {
       'badge--disabled': !this.state.enabled
     })
     return (
-      <div 
+      <button 
         className={styleClasses} 
         onClick={this.onClick.bind(this)} 
         onMouseEnter={this.onMouseEnter.bind(this)}
         onMouseLeave={this.onMouseLeave.bind(this)}
         >
         {this.props.caption}
-      </div>
+      </button>
     )
   }
 
